4d1d82a3145dc4bf9df1be77d26dc943b1aae553
Christophe <61232559+CLabeeu123@users.noreply.github.com>
2022-03-18 12:54:58 +0100

    Add files via upload

---
+++ RecEngine/RecEngine/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace RecEngine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 361);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(345, 59);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Recommend!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button_click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 98);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(345, 28);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBox2.Enter += new System.EventHandler(this.txt_Enter);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("MS Reference Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(12, 13);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(345, 79);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.Text = "This is a recommandation engine for Christmas gifts. Type a text in the textbox b" +
+    "elow to find out what gifts it can recommend for you.";
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 132);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(97, 56);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "Gender";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(135, 132);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(97, 56);
+            this.textBox4.TabIndex = 5;
+            this.textBox4.Text = "Positive or Negative";
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox5.Location = new System.Drawing.Point(256, 132);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(97, 56);
+            this.textBox5.TabIndex = 6;
+            this.textBox5.Text = "Gift Ideas";
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(12, 194);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(341, 161);
+            this.textBox6.TabIndex = 7;
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(365, 432);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Recommendation Engine";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+    }
+}
+
---
+++ RecEngine/RecEngine/Form1.cs
@@ -0,0 +1,173 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using HtmlDocument = HtmlAgilityPack.HtmlDocument;
+
+namespace RecEngine
+{
+    public partial class Form1 : Form
+    {
+        public string sampletext = "Type here a small text";
+        public bool editsampletext;
+        public Form1()
+        {
+            InitializeComponent();
+            textBox2.Text = sampletext;
+            textBox2.ForeColor = Color.Black;
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void txt_Enter(object sender, EventArgs e)
+        {
+            if (!editsampletext)
+            {
+                textBox2.Clear();
+                textBox2.ForeColor = Color.Black;
+            }
+        }
+        private void button_click(object sender, EventArgs e)
+        {
+            makeFile();
+            
+            string[] lines = File.ReadLines("C:\\ChristmasList.txt").ToArray();
+            string checkGender = checkMaleOrFemale(lines[0]);
+            textBox1.Text = checkGender;
+
+            string posOrNeg = isWordNegativeOrPositive(lines[1]);
+            textBox4.Text = posOrNeg;
+
+            List<string> gift = new List<string>(lines);
+            gift.RemoveAt(0);
+            gift.RemoveAt(0);
+
+            string result = String.Join(" ", gift.ToArray());
+            textBox5.Text = result;
+
+            string finalgift;
+            
+            if (textBox4.Text == "Negative")
+            {
+                string neg = textBox1.Text;
+                finalgift = scrapeAmazon(neg);
+                textBox6.Text = finalgift;
+            }
+            else
+            {
+                finalgift = scrapeAmazon(result);
+                textBox6.Text = finalgift;
+            }
+
+
+        }
+        public void makeFile()
+        {
+            StreamWriter sw = new StreamWriter("C:\\ChristmasList.txt");
+
+            string words = textBox2.Text;
+
+            string[] space = words.Split(' ');
+
+            foreach (var item in space)
+            {
+                sw.WriteLine(item);
+            }
+
+            sw.Close();
+        }
+        public string checkMaleOrFemale(string gender)
+        {
+            string data = getAPIDataForGender(gender);
+            List<string> info = parseDataForGender(data);
+            return info[0];
+        }
+        public string isWordNegativeOrPositive(string word)
+        {
+            string[] posWords = {"like","likes", "love", "loves", "adore", "adores", 
+                 "want", "wants", "need", "needs", "enjoy", "enjoys", "prefer", "prefers" };
+
+            if (posWords.Contains(word))
+            {
+                return "Positive";
+            }
+            else
+            {
+                return "Negative";
+            }
+        }
+        private static string getAPIDataForGender(string name)
+        {
+            WebRequest wr = WebRequest.Create("https://api.genderize.io?name=" + name);
+            WebResponse response = wr.GetResponse();
+            System.IO.Stream stream = response.GetResponseStream();
+            System.IO.StreamReader dataReader = new System.IO.StreamReader(stream);
+            string allData = dataReader.ReadToEnd();
+            return allData;
+        }
+        private static List<String> parseDataForGender(string data)
+        {
+            List<String> information = new List<String>();
+            dynamic jsonData = JObject.Parse(data);
+            string probableGender = jsonData.gender;
+            information.Add(probableGender);
+            return information;
+        }
+        public static string scrapeAmazon(string gift)
+        {
+            HtmlAgilityPack.HtmlWeb website = new HtmlAgilityPack.HtmlWeb();
+            HtmlAgilityPack.HtmlDocument document = website.Load("https://www.amazon.nl/s?k=" + gift + " gift");
+            var datalist = document.DocumentNode.SelectNodes("//span[@class='a-size-base-plus a-color-base a-text-normal']").ToList();
+            string url = "https://www.amazon.nl/s?k=" + gift + " gift";
+
+
+            foreach (var item in datalist)
+            {
+                if (true)
+                {
+                    string s = datalist[0].InnerHtml;
+                    string ss = datalist[1].InnerHtml;
+                    string sss = datalist[2].InnerHtml;
+
+                    return "1) " + s + "---------------" + "2) " + ss + "---------------" + "3) " + sss + "---------------" + url;
+
+
+                }
+                
+            }
+            return "nothing found";
+        }
+    }
+}
